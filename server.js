c3d2493108f031b3ef469e67dca87ab641dd5c20
Ajay Ghiyad <73992468+IamGodseye@users.noreply.github.com>
2021-12-20 17:05:13 +0530

    first commit

--- server.js
+++ server.js
@@ -12,14 +12,9 @@ const app = express();
 const csrfProtection = csrf({ cookie: true });
 //db connection
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    userUnifiedToplogoy: true,
-    useCreateIndex: true,
-  })
+  .connect(process.env.DATABASE, {})
   .then(() => console.log("DB Connected"))
-  .catch(() => console.log("DB Connection Err=>", err));
+  .catch((err) => console.log("DB Connection Err=>", err));
 //apply middle-wares
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
