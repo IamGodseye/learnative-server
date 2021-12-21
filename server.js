d5d17120a1ac4c0692ee3a64b50f99ed52cc79cf
Ajay Ghiyad <73992468+IamGodseye@users.noreply.github.com>
2021-12-21 14:31:05 +0530

    options #2

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ mongoose
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log("DB Connection Err=>", err));
 //apply middle-wares
+app.use(cors());
+
+app.use(express.json({ limit: "5mb" }));
+app.use(morgan("dev"));
+app.use(cookieParser());
+// app.use((req, res, next) => {
+//   console.log("hehe my middleware");
+//   next();
+// });
+//route
 app.options("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
@@ -25,15 +35,7 @@ app.options("/*", function (req, res, next) {
   );
   res.send(200);
 });
-app.use(cors());
-app.use(express.json({ limit: "5mb" }));
-app.use(morgan("dev"));
-app.use(cookieParser());
-// app.use((req, res, next) => {
-//   console.log("hehe my middleware");
-//   next();
-// });
-//route
+
 readdirSync("./routes").map((r) => {
   app.use("/api", require(`./routes/${r}`));
 });
