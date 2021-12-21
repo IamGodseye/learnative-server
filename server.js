9c269a5ca30066b04f66b7263e87b543d3a253db
Ajay Ghiyad <73992468+IamGodseye@users.noreply.github.com>
2021-12-21 14:12:08 +0530

    cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+const router = express.Router();
+router.options(cors());
 const morgan = require("morgan");
 require("dotenv").config();
 import mongoose from "mongoose";
