48a7a215fa57506fe72ee40f7bc43d9b4f83b000
Ajay Ghiyad <73992468+IamGodseye@users.noreply.github.com>
2021-12-21 14:33:50 +0530

    options #3

--- server.js
+++ server.js
@@ -26,14 +26,22 @@ app.use(cookieParser());
 //   next();
 // });
 //route
-app.options("/*", function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, Content-Length, X-Requested-With"
   );
-  res.send(200);
+
+  //intercepts OPTIONS method
+  if ("OPTIONS" === req.method) {
+    //respond with 200
+    res.send(200);
+  } else {
+    //move on
+    next();
+  }
 });
 
 readdirSync("./routes").map((r) => {
