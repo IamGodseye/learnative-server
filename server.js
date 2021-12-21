209d4a44bf10febcf4d8a7669e36b257491a5b72
Ajay Ghiyad <73992468+IamGodseye@users.noreply.github.com>
2021-12-21 15:07:06 +0530

    removed options

--- server.js
+++ server.js
@@ -26,23 +26,7 @@ app.use(cookieParser());
 //   next();
 // });
 //route
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
 
-  //intercepts OPTIONS method
-  if ("OPTIONS" === req.method) {
-    //respond with 200
-    res.send(200);
-  } else {
-    //move on
-    next();
-  }
-});
 readdirSync("./routes").map((r) => {
   app.use("/api", require(`./routes/${r}`));
 });
