7059e4a6c768e3f95a03d5e887856022db9e89f0
Ajay Ghiyad <73992468+IamGodseye@users.noreply.github.com>
2021-12-22 21:51:12 +0530

    cookie update

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,19 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 
-router.get("/current-user", requireSignin, currentUser);
+router.get(
+  "/current-user",
+  // (req, res, next) => {
+  //   console.log(req.cookies.token, " \n");
+  //   console.log(req.headers.authorization);
+  //   next();
+  // },
+  requireSignin,
+  () => {
+    console.log("Autho op");
+  },
+  currentUser
+);
 router.get("/send-email", sendTestEmail);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
